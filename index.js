b5d8851175025d9e2f70bc278d2f743607df0de1
Arpan-kumawat <kumawatarpan10@gmail.com>
2023-01-23 00:31:03 +0530

    234

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express"
+ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import food from "./Schema.js"
@@ -8,6 +8,10 @@ const port =process.env.PORT || 9002
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
 
 
 
